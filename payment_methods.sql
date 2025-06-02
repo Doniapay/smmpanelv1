fa1d535526ca9681cb0fe68206846b8b7048b553
Doniapay <sattikgamer409@gmail.com>
2025-06-02 16:32:54 +0600

    Add files via upload

---
+++ payment_methods.sql
@@ -0,0 +1,4 @@
+INSERT INTO `payment_methods` 
+(`id`, `method_name`, `method_get`, `method_min`, `method_max`, `method_type`, `method_extras`, `method_line`) 
+VALUES 
+(133, 'doniapay', 'doniapay', 1, 1000, '2', '{\"method_type\":\"1\",\"name\":\"doniapay\",\"min\":\"1\",\"max\":\"10000\",\"api_key\":\"12345\",\"exchange_rate\":\"100\"}', 1);
